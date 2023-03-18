1895a6eac29a4ac44b9a883d6bedb0d6f318556d
Manuel Kosovel <kosovelmanuel@gmail.com>
2023-03-18 13:19:14 +0100

    Initial commit

---
+++ app/src/main/java/it/units/img_processing_studemt_app/MainActivity.java
@@ -0,0 +1,14 @@
+package it.units.img_processing_studemt_app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
